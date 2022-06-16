66cf8e62d86d4ea78e9da3cc501243e7f9f7e082
Soumya1998-18 <107382070+Soumya1998-18@users.noreply.github.com>
2022-06-16 11:20:02 +0530

    Add files via upload
    React Router Components.

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <>
+  
+    <ul>
+      <li><NavLink to="/" style={({isActive})=>{return{backgroundColor: isActive ? 'red': ''}}}>Home</NavLink></li>
+      <li><NavLink to="/about" style={({isActive})=>{return{backgroundColor: isActive ? 'red': ''}}}>About</NavLink></li>
+      <li><NavLink to="/contact" style={({isActive})=>{return{backgroundColor: isActive ? 'red': ''}}}>Contact</NavLink></li>
+      <li><NavLink to="/post/mobile" style={({isActive})=>{return{backgroundColor: isActive ? 'red': ''}}}>Post</NavLink></li>
+      <li><NavLink to="/post/mobile/1" style={({isActive})=>{return{backgroundColor: isActive ? 'red': ''}}}>Post with ID</NavLink></li>
+    </ul>
+    </>
+  );
+};
+
+export default Navbar;
